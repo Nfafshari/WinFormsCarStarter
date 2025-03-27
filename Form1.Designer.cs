023a43859ad35546f834d4bf0f999251ca98ef41
NathenAfshari <nathen.afshari@gmail.com>
2025-03-27 13:37:24 -0500

    Added Color to Active Tab

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,10 +28,147 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            panel1 = new Panel();
+            button_profile = new Button();
+            button_trips = new Button();
+            button_home = new Button();
+            button_status = new Button();
+            button_activity = new Button();
+            panel_activity = new Panel();
+            panel_status = new Panel();
+            panel_home = new Panel();
+            panel_trips = new Panel();
+            panel_profile = new Panel();
+            panel1.SuspendLayout();
+            panel_activity.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.White;
+            panel1.Controls.Add(button_profile);
+            panel1.Controls.Add(button_trips);
+            panel1.Controls.Add(button_home);
+            panel1.Controls.Add(button_status);
+            panel1.Controls.Add(button_activity);
+            panel1.Dock = DockStyle.Bottom;
+            panel1.Location = new Point(0, 438);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(265, 46);
+            panel1.TabIndex = 0;
+            // 
+            // button_profile
+            // 
+            button_profile.FlatStyle = FlatStyle.Flat;
+            button_profile.Location = new Point(211, 0);
+            button_profile.Name = "button_profile";
+            button_profile.Size = new Size(54, 46);
+            button_profile.TabIndex = 4;
+            button_profile.Text = "Profile";
+            button_profile.UseVisualStyleBackColor = true;
+            button_profile.Click += button_profile_Click;
+            // 
+            // button_trips
+            // 
+            button_trips.FlatStyle = FlatStyle.Flat;
+            button_trips.Location = new Point(157, 0);
+            button_trips.Name = "button_trips";
+            button_trips.Size = new Size(56, 46);
+            button_trips.TabIndex = 3;
+            button_trips.Text = "Trips";
+            button_trips.UseVisualStyleBackColor = true;
+            button_trips.Click += button_trips_Click;
+            // 
+            // button_home
+            // 
+            button_home.FlatStyle = FlatStyle.Flat;
+            button_home.Location = new Point(106, 0);
+            button_home.Name = "button_home";
+            button_home.Size = new Size(53, 46);
+            button_home.TabIndex = 2;
+            button_home.Text = "Home";
+            button_home.UseVisualStyleBackColor = true;
+            button_home.Click += button_home_Click;
+            // 
+            // button_status
+            // 
+            button_status.FlatStyle = FlatStyle.Flat;
+            button_status.Location = new Point(54, 0);
+            button_status.Name = "button_status";
+            button_status.Size = new Size(54, 46);
+            button_status.TabIndex = 1;
+            button_status.Text = "Status";
+            button_status.UseVisualStyleBackColor = true;
+            button_status.Click += button_status_Click;
+            // 
+            // button_activity
+            // 
+            button_activity.FlatStyle = FlatStyle.Flat;
+            button_activity.Location = new Point(0, 0);
+            button_activity.Name = "button_activity";
+            button_activity.Size = new Size(55, 46);
+            button_activity.TabIndex = 0;
+            button_activity.Text = "Activity";
+            button_activity.UseVisualStyleBackColor = true;
+            button_activity.Click += button_activity_Click;
+            // 
+            // panel_activity
+            // 
+            panel_activity.Controls.Add(panel_status);
+            panel_activity.Dock = DockStyle.Fill;
+            panel_activity.Location = new Point(0, 0);
+            panel_activity.Name = "panel_activity";
+            panel_activity.Size = new Size(265, 438);
+            panel_activity.TabIndex = 1;
+            // 
+            // panel_status
+            // 
+            panel_status.Dock = DockStyle.Fill;
+            panel_status.Location = new Point(0, 0);
+            panel_status.Name = "panel_status";
+            panel_status.Size = new Size(265, 438);
+            panel_status.TabIndex = 0;
+            // 
+            // panel_home
+            // 
+            panel_home.Dock = DockStyle.Fill;
+            panel_home.Location = new Point(0, 0);
+            panel_home.Name = "panel_home";
+            panel_home.Size = new Size(265, 438);
+            panel_home.TabIndex = 2;
+            // 
+            // panel_trips
+            // 
+            panel_trips.Dock = DockStyle.Fill;
+            panel_trips.Location = new Point(0, 0);
+            panel_trips.Name = "panel_trips";
+            panel_trips.Size = new Size(265, 438);
+            panel_trips.TabIndex = 3;
+            // 
+            // panel_profile
+            // 
+            panel_profile.Dock = DockStyle.Fill;
+            panel_profile.Location = new Point(0, 0);
+            panel_profile.Name = "panel_profile";
+            panel_profile.Size = new Size(265, 438);
+            panel_profile.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(265, 484);
+            Controls.Add(panel_profile);
+            Controls.Add(panel_trips);
+            Controls.Add(panel_home);
+            Controls.Add(panel_activity);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            Name = "Form1";
+            Text = "Piper Auto Start";
+            panel1.ResumeLayout(false);
+            panel_activity.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,167 @@
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
 namespace WinFormsCarStarter
 {
     public partial class Form1 : Form
     {
+        private ImageList imageList = new ImageList();
+        private ImageList active_imageList = new ImageList();
+
         public Form1()
         {
             InitializeComponent();
+
+
+            /******** Tabbed Interface Setup ********/
+            // Tab setup using panels
+            panel_activity.Dock = DockStyle.Fill;
+            panel_status.Dock = DockStyle.Fill;
+            panel_home.Dock = DockStyle.Fill;
+            panel_trips.Dock = DockStyle.Fill;
+            panel_profile.Dock = DockStyle.Fill;
+
+            // Button appearance
+            button_activity.FlatAppearance.BorderSize = 0;
+            button_status.FlatAppearance.BorderSize = 0;
+            button_home.FlatAppearance.BorderSize = 0;
+            button_trips.FlatAppearance.BorderSize = 0;
+            button_profile.FlatAppearance.BorderSize = 0;
+
+            button_activity.FlatAppearance.MouseOverBackColor = Color.LightGray;
+            button_status.FlatAppearance.MouseOverBackColor = Color.LightGray;
+            button_home.FlatAppearance.MouseOverBackColor = Color.LightGray;
+            button_trips.FlatAppearance.MouseOverBackColor = Color.LightGray;
+            button_profile.FlatAppearance.MouseOverBackColor = Color.LightGray;
+
+            button_activity.FlatAppearance.MouseDownBackColor = Color.Gray;
+            button_status.FlatAppearance.MouseDownBackColor = Color.Gray;
+            button_home.FlatAppearance.MouseDownBackColor = Color.Gray;
+            button_trips.FlatAppearance.MouseDownBackColor = Color.Gray;
+            button_profile.FlatAppearance.MouseDownBackColor = Color.Gray;
+
+            // Button text
+            button_activity.TextAlign = ContentAlignment.BottomCenter;
+            button_status.TextAlign = ContentAlignment.BottomCenter;
+            button_home.TextAlign = ContentAlignment.BottomCenter;
+            button_trips.TextAlign = ContentAlignment.BottomCenter;
+            button_profile.TextAlign = ContentAlignment.BottomCenter;
+
+
+            // Button Icons
+            ImageList imageList = new ImageList();
+            imageList.Images.Add("activity", Image.FromFile("icons\\activity.png"));
+            imageList.Images.Add("status", Image.FromFile("icons\\status.png"));
+            imageList.Images.Add("home", Image.FromFile("icons\\home.png"));
+            imageList.Images.Add("trips", Image.FromFile("icons\\nav.png"));
+            imageList.Images.Add("profile", Image.FromFile("icons\\profile.png"));
+            imageList.Images.Add("activated activity", Image.FromFile("icons\\active_activity.png"));
+            imageList.Images.Add("activated status", Image.FromFile("icons\\active_status.png"));
+            imageList.Images.Add("activated home", Image.FromFile("icons\\active_house.png"));
+            imageList.Images.Add("activated trips", Image.FromFile("icons\\active_nav.png"));
+            imageList.Images.Add("activated profile", Image.FromFile("icons\\active_profile.png"));
+
+            button_activity.ImageList = imageList;
+            button_status.ImageList = imageList;
+            button_home.ImageList = imageList;
+            button_trips.ImageList = imageList;
+            button_profile.ImageList = imageList;
+
+            button_activity.ImageIndex = 0;
+            button_status.ImageIndex = 1;
+            button_home.ImageIndex = 2;
+            button_trips.ImageIndex = 3;
+            button_profile.ImageIndex = 4;
+
+            button_activity.ImageAlign = ContentAlignment.TopCenter;
+            button_status.ImageAlign = ContentAlignment.TopCenter;
+            button_home.ImageAlign = ContentAlignment.TopCenter;
+            button_trips.ImageAlign = ContentAlignment.TopCenter;
+            button_profile.ImageAlign = ContentAlignment.TopCenter;
+
+            // Show home tab on startup
+            ShowTab(panel_home);
+        }
+
+        /************** GLOBAL METHODS *************/
+        // ShowTab -- makes the tab (panel) visible depending on which button is clicked
+        private void ShowTab(Panel visibleTab)
+        {
+            // Hide all tabs
+            panel_activity.Visible = false;
+            panel_status.Visible = false;
+            panel_home.Visible = false;
+            panel_trips.Visible = false;
+            panel_profile.Visible = false;
+
+            // Show parameter tab
+            visibleTab.Visible = true;
+        }
+
+
+        /******************* BUTTON METHODS ************************/
+        // ActiveTab -- shows the user what tab they are currently on 
+        private void ActiveTab (Button activeTab)
+        {
+            // Set all tabs text one color
+            button_activity.ForeColor = Color.Black;
+            button_status.ForeColor = Color.Black;
+            button_home.ForeColor = Color.Black;
+            button_trips.ForeColor = Color.Black;
+            button_profile.ForeColor = Color.Black;
+
+            // Set all icons to one color
+            button_activity.ImageIndex = 0;
+            button_status.ImageIndex = 1;
+            button_home.ImageIndex = 2;
+            button_trips.ImageIndex = 3;
+            button_profile.ImageIndex = 4;
+
+
+            // Set active text color
+            activeTab.ForeColor = Color.MediumPurple;
+
+            // Change the icon of the active button
+            if (activeTab == button_activity)
+                button_activity.ImageIndex = 5;
+            else if (activeTab == button_status)
+                button_status.ImageIndex = 6;
+            else if (activeTab == button_home)
+                button_home.ImageIndex = 7;
+            else if (activeTab == button_trips)
+                button_trips.ImageIndex = 8;
+            else if (activeTab == button_profile)
+                button_profile.ImageIndex = 9;
+        }
+
+        private void button_activity_Click(object sender, EventArgs e)
+        {
+            ShowTab(panel_activity);
+            ActiveTab(button_activity);
+        }
+
+        private void button_status_Click(object sender, EventArgs e)
+        {
+            ShowTab(panel_status);
+            ActiveTab(button_status);
+        }
+
+        private void button_home_Click(object sender, EventArgs e)
+        {
+            ShowTab(panel_home);
+            ActiveTab(button_home);
+        }
+
+        private void button_trips_Click(object sender, EventArgs e)
+        {
+            ShowTab(panel_trips);
+            ActiveTab(button_trips);
+        }
+
+        private void button_profile_Click(object sender, EventArgs e)
+        {
+            ShowTab(panel_profile);
+            ActiveTab(button_profile); 
         }
     }
 }

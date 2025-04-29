f9fb642a4be18e70ffd3e9159886c1f2f03863b7
NathenAfshari <nathen.afshari@gmail.com>
2025-04-29 09:10:19 -0500

    Edited UI

--- MainForm.cs
+++ MainForm.cs
@@ -857,7 +857,7 @@ namespace WinFormsCarStarter
             panel_trips.Controls.Add(comboBox_tripDate);
 
             // *********************************** PROFILE TAB ******************************** //
-            panel_trips.BackColor = Color.White;
+            panel_profile.BackColor = Color.White;
 
             Label label_profile = new Label()
             {
@@ -871,7 +871,7 @@ namespace WinFormsCarStarter
             flowLayoutPanel_profile = new FlowLayoutPanel()
             {
                 Size = new Size(panel_status.Width, 400),
-                Location = new Point(Left, 40),
+                Location = new Point(Left, 45),
                 BackColor = Color.White,
                 FlowDirection = FlowDirection.TopDown,
                 AutoScroll = true,
@@ -898,7 +898,8 @@ namespace WinFormsCarStarter
                 AutoSize = true,
                 ForeColor = Color.Black,
                 Location = new Point(70, 130),
-                TextAlign = ContentAlignment.MiddleCenter
+                TextAlign = ContentAlignment.MiddleCenter,
+                Margin = new Padding(40, 0, 0, 0)
             };
             label_fullName.Left = (panel_profile.Width - label_fullName.Width) / 2;
             flowLayoutPanel_profile.Controls.Add(label_fullName);
@@ -911,7 +912,8 @@ namespace WinFormsCarStarter
                 Font = new Font("Segoe UI", 10, FontStyle.Regular),
                 ForeColor = Color.Gray,
                 Location = new Point((panel_profile.Width - 200) / 2, 165),
-                TextAlign = ContentAlignment.MiddleCenter
+                TextAlign = ContentAlignment.MiddleCenter,
+                Margin = new Padding(80, 0, 0, 0)
             };
             label_profileEmail.Left = (panel_profile.Width - label_profileEmail.Width) / 2;
             flowLayoutPanel_profile.Controls.Add(label_profileEmail);
@@ -922,7 +924,8 @@ namespace WinFormsCarStarter
                 BackColor = Color.LightGray,
                 Height = 1,
                 Width = 200,
-                Location = new Point((panel_profile.Width - 200) / 2, 200)
+                Location = new Point((panel_profile.Width - 200) / 2, 200),
+                Margin = new Padding(40, 0, 0, 0)
             };
             flowLayoutPanel_profile.Controls.Add(divider);
 
@@ -933,6 +936,7 @@ namespace WinFormsCarStarter
                 AutoSize = true,
                 Font = new Font("Segoe UI", 10, FontStyle.Regular),
                 Location = new Point(50, 230),
+                Margin = new Padding(40, 0, 0, 0)
             };
             flowLayoutPanel_profile.Controls.Add(label_vehicleType);
 
@@ -942,6 +946,7 @@ namespace WinFormsCarStarter
                 AutoSize = true,
                 Font = new Font("Segoe UI", 10, FontStyle.Regular),
                 Location = new Point(50, 260),
+                Margin = new Padding(40, 0, 0, 0)
             };
             flowLayoutPanel_profile.Controls.Add(label_profileVin);
 
@@ -954,7 +959,8 @@ namespace WinFormsCarStarter
                 BackColor = Color.MediumPurple,
                 ForeColor = Color.White,
                 FlatStyle = FlatStyle.Flat,
-                Font = new Font("Segoe UI", 10, FontStyle.Bold)
+                Font = new Font("Segoe UI", 10, FontStyle.Bold),
+                Margin = new Padding(40, 0, 10, 0)
             };
             button_editProfile.FlatAppearance.BorderSize = 2;
             button_editProfile.FlatAppearance.BorderColor = Color.Black;
@@ -970,7 +976,8 @@ namespace WinFormsCarStarter
                 BackColor = Color.Red,
                 ForeColor = Color.White,
                 FlatStyle = FlatStyle.Flat,
-                Font = new Font("Segoe UI", 10, FontStyle.Bold)
+                Font = new Font("Segoe UI", 10, FontStyle.Bold),
+                Margin = new Padding(40, 0, 10, 0)
             };
             button_logout.FlatAppearance.BorderSize = 2;
             button_logout.FlatAppearance.BorderColor = Color.Black;
